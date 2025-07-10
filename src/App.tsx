c2c3767f8610dc03ed68e4f77efee84d698b4af4
William Roger <williamroger.frontend@gmail.com>
2025-07-10 18:30:51 -0300

    utilização basica do react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { useQuery, QueryClient } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+interface IUser {
+  id: string;
+  name: string;
+  email: string;
+}
+
 export function App() {
+  const { data } = useQuery({
+    queryKey: ['users'],
+    queryFn: async (): Promise<IUser[]> => {
+      const response = await fetch('http://localhost:3000/users');
+      return response.json();
+    }
+  }, queryClient);
+
   return (
-    <h1>Hello Word!</h1>
+    <div>
+      {data?.map((user) => (
+      <div key={user.id}>
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </div>
+    ))}
+    </div>
   )
 }
