94cc2d2f2e6b7589990a1c5965e6cb6d05bfb19c
William Roger <williamroger.frontend@gmail.com>
2025-07-11 19:29:46 -0300

    adicionado reac-query-dev-tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Users } from './Users';
 
 const queryClient = new QueryClient();
@@ -8,6 +8,7 @@ export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Users />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   )
 }
